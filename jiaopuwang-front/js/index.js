0871d2f10b43e0ab58e5d3097c0d106df0b0c550
zhuxiaoyun <1115905543@qq.com>
2016-03-17 15:04:53 +0800

    commit by zhuxiaoyun on 3.17

--- jiaopuwang-front/js/index.js
+++ jiaopuwang-front/js/index.js
@@ -5,4 +5,18 @@ $(function(){
 	$(".topbar_2_btn_self").mouseout(function() {
 		$(this).removeClass("topbar_2_btn_self_hover");
 	});
+
+	$(".tab_li").click(function() {
+		$(".am-active").removeClass("am-active");
+		$(this).addClass("am-active");
+		
+	});
+
+	$(".hot_lables_btn").click(function() {
+		$(this).toggleClass("hot_lables_btn_active");
+	});	
+	$(".hot_lables_btn_active").click(function() {
+		$(this).toggleClass("hot_lables_btn");
+	});
+
 })
