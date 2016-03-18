b038c86efe683ea7757e99b0f7b9dd368c4a527a
zhuxiaoyun <1115905543@qq.com>
2016-03-18 17:43:33 +0800

    commit by zhuxiaoyun on 03-18

---
+++ jiaopuwang-front/js/recruitment.js
@@ -0,0 +1,9 @@
+$(function(){
+	$(".topbar_2_btn_self").mouseover(function() {
+		$(this).addClass("topbar_2_btn_self_hover");
+	});
+	$(".topbar_2_btn_self").mouseout(function() {
+		$(this).removeClass("topbar_2_btn_self_hover");
+	});
+	
+})
